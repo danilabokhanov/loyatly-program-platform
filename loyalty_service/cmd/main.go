d442110dd56b84f41453a3fee84414bb3f108311
danilabohanov <danilabohanov@gmail.com>
2025-04-03 22:32:38 +0300

    fix

--- loyalty_service/cmd/main.go
+++ loyalty_service/cmd/main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
+	proto "loyaltyservice/proto"
 	"net"
-	"promo"
 	"time"
 
 	"github.com/gocql/gocql"
+	"github.com/golang/protobuf/ptypes/empty"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type promoServer struct {
-	promo.UnimplementedPromoServiceServer
+	proto.UnimplementedPromoServiceServer
 	session *gocql.Session
 }
 
-func (s *promoServer) CreatePromo(ctx context.Context, req *promo.CreatePromoRequest) (*promo.Promo, error) {
+func (s *promoServer) CreatePromo(ctx context.Context, req *proto.CreatePromoRequest) (*proto.Promo, error) {
 	id := gocql.TimeUUID()
 	creationTime := time.Now()
 	if err := s.session.Query(
@@ -27,7 +29,7 @@ func (s *promoServer) CreatePromo(ctx context.Context, req *promo.CreatePromoReq
 	).Exec(); err != nil {
 		return nil, err
 	}
-	return &promo.Promo{
+	return &proto.Promo{
 		Id:           id.String(),
 		Title:        req.Title,
 		Description:  req.Description,
@@ -39,8 +41,8 @@ func (s *promoServer) CreatePromo(ctx context.Context, req *promo.CreatePromoReq
 	}, nil
 }
 
-func (s *promoServer) GetPromo(ctx context.Context, req *promo.GetPromoRequest) (*promo.Promo, error) {
-	var p promo.Promo
+func (s *promoServer) GetPromo(ctx context.Context, req *proto.GetPromoRequest) (*proto.Promo, error) {
+	var p proto.Promo
 	var creationDate, updateDate time.Time
 	if err := s.session.Query(
 		"SELECT id, title, description, author_id, discount_rate, promo_code, creation_date, update_date FROM promos WHERE id = ? LIMIT 1",
@@ -53,7 +55,17 @@ func (s *promoServer) GetPromo(ctx context.Context, req *promo.GetPromoRequest) 
 	return &p, nil
 }
 
-func (s *promoServer) UpdatePromo(ctx context.Context, req *promo.UpdatePromoRequest) (*promo.Promo, error) {
+func (s *promoServer) UpdatePromo(ctx context.Context, req *proto.UpdatePromoRequest) (*proto.Promo, error) {
+	var authorId string
+	err := s.session.Query("SELECT author_id FROM promos WHERE id = ?", req.Id).Scan(&authorId)
+	if err != nil {
+		return nil, fmt.Errorf("promo not found or database error: %v", err)
+	}
+
+	if authorId != req.AuthorId {
+		return nil, fmt.Errorf("permission denied: only the author can update this promo")
+	}
+
 	updateTime := time.Now()
 	if err := s.session.Query(
 		"UPDATE promos SET title = ?, description = ?, discount_rate = ?, update_date = ? WHERE id = ?",
@@ -61,21 +73,33 @@ func (s *promoServer) UpdatePromo(ctx context.Context, req *promo.UpdatePromoReq
 	).Exec(); err != nil {
 		return nil, err
 	}
-	return s.GetPromo(ctx, &promo.GetPromoRequest{Id: req.Id})
+
+	return s.GetPromo(ctx, &proto.GetPromoRequest{Id: req.Id})
 }
 
-func (s *promoServer) DeletePromo(ctx context.Context, req *promo.DeletePromoRequest) (*promo.Promo, error) {
+func (s *promoServer) DeletePromo(ctx context.Context, req *proto.DeletePromoRequest) (*empty.Empty, error) {
+	var authorId string
+	err := s.session.Query("SELECT author_id FROM promos WHERE id = ?", req.Id).Scan(&authorId)
+	if err != nil {
+		return nil, fmt.Errorf("promo not found or database error: %v", err)
+	}
+
+	if authorId != req.AuthorId {
+		return nil, fmt.Errorf("permission denied: only the author can delete this promo")
+	}
+
 	if err := s.session.Query("DELETE FROM promos WHERE id = ?", req.Id).Exec(); err != nil {
 		return nil, err
 	}
-	return &promo.Promo{}, nil
+
+	return &empty.Empty{}, nil
 }
 
-func (s *promoServer) ListPromos(ctx context.Context, req *promo.ListPromosRequest) (*promo.ListPromosResponse, error) {
-	var promos []*promo.Promo
+func (s *promoServer) ListPromos(ctx context.Context, req *proto.ListPromosRequest) (*proto.ListPromosResponse, error) {
+	var promos []*proto.Promo
 	iter := s.session.Query("SELECT id, title, description, author_id, discount_rate, promo_code, creation_date, update_date FROM promos").Iter()
 	for {
-		var p promo.Promo
+		var p proto.Promo
 		var creationDate, updateDate time.Time
 		if !iter.Scan(&p.Id, &p.Title, &p.Description, &p.AuthorId, &p.DiscountRate, &p.PromoCode, &creationDate, &updateDate) {
 			break
@@ -87,26 +111,88 @@ func (s *promoServer) ListPromos(ctx context.Context, req *promo.ListPromosReque
 	if err := iter.Close(); err != nil {
 		return nil, err
 	}
-	return &promo.ListPromosResponse{Promos: promos}, nil
+	return &proto.ListPromosResponse{Promos: promos}, nil
 }
 
-func connectToCassandra() *gocql.Session {
-	cluster := gocql.NewCluster("cassandra")
-	cluster.Keyspace = "promo_service"
+func connectToCassandra(host string, port int, keyspace string) *gocql.Session {
+	cluster := gocql.NewCluster(host)
+	cluster.Port = port
 	cluster.Consistency = gocql.Quorum
+	cluster.ConnectTimeout = 10 * time.Second
+	cluster.Timeout = 10 * time.Second
+
 	var session *gocql.Session
 	var err error
-	for i := 0; i < 10; i++ {
+
+	maxRetries := 10
+	retryCount := 0
+
+	for retryCount < maxRetries {
 		session, err = cluster.CreateSession()
 		if err == nil {
 			log.Println("Connected to Cassandra")
-			return session
+			break
 		}
+
 		log.Println("Waiting for Cassandra to be ready...")
 		time.Sleep(5 * time.Second)
+		retryCount++
 	}
-	log.Fatal("Failed to connect to Cassandra:", err)
-	return nil
+
+	if err != nil {
+		log.Fatal("Tries limit exceed ...")
+		return nil
+	}
+
+	var count int
+	err = session.Query("SELECT COUNT(*) FROM system_schema.keyspaces WHERE keyspace_name = ?", keyspace).Scan(&count)
+	if err != nil {
+		session.Close()
+		log.Fatal("failed to check system_schema.keyspaces: %v", err)
+		return nil
+	}
+
+	if count == 0 {
+		log.Printf("key %s not found. Creating...", keyspace)
+		createKeyspaceQuery := fmt.Sprintf(`
+	  CREATE KEYSPACE %s
+	  WITH replication = {
+	   'class': 'SimpleStrategy',
+	   'replication_factor': 1
+	  }
+	 `, keyspace)
+
+		err = session.Query(createKeyspaceQuery).Exec()
+		if err != nil {
+			session.Close()
+			log.Fatalf(": %v", err)
+			return nil
+		}
+	}
+
+	session.Close()
+
+	cluster.Keyspace = keyspace
+
+	retryCount = 0
+	for retryCount < maxRetries {
+		session, err = cluster.CreateSession()
+		if err == nil {
+			break
+		}
+
+		log.Println("Waiting for Cassandra to be ready...")
+		time.Sleep(5 * time.Second)
+		retryCount++
+	}
+
+	if err != nil {
+		log.Fatal("Tries limit exceed ...")
+		return nil
+	}
+
+	log.Println("Cassandra is ok")
+	return session
 }
 
 func initializeDatabase(session *gocql.Session) {
@@ -126,21 +212,21 @@ func initializeDatabase(session *gocql.Session) {
 }
 
 func main() {
-	session := connectToCassandra()
+	session := connectToCassandra("cassandra", 9042, "loyalty_service")
 	defer session.Close()
 
 	initializeDatabase(session)
 
 	server := grpc.NewServer()
-	promo.RegisterPromoServiceServer(server, &promoServer{session: session})
+	proto.RegisterPromoServiceServer(server, &promoServer{session: session})
 	reflection.Register(server)
 
-	listener, err := net.Listen("tcp", ":50051")
+	listener, err := net.Listen("tcp", ":8083")
 	if err != nil {
 		log.Fatal("Failed to listen:", err)
 	}
 
-	log.Println("gRPC server started on :50051")
+	log.Println("gRPC server started on :8083")
 	if err := server.Serve(listener); err != nil {
 		log.Fatal("Failed to serve:", err)
 	}
--- loyalty_service/go.mod
+++ loyalty_service/go.mod
@@ -1,23 +1,24 @@
-module authservice
+module loyaltyservice
 
 go 1.23.0
 
 toolchain go1.24.0
 
-require github.com/google/uuid v1.6.0
+require (
+	github.com/gocql/gocql v1.7.0
+	github.com/golang/protobuf v1.5.4
+	google.golang.org/grpc v1.71.1
+	google.golang.org/protobuf v1.36.6
+)
 
 require (
-	github.com/go-chi/chi/v5 v5.2.1
-	github.com/golang-jwt/jwt v3.2.2+incompatible
-	github.com/jackc/pgpassfile v1.0.0 // indirect
-	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
-	github.com/jackc/pgx/v5 v5.7.2 // indirect
-	github.com/jackc/puddle/v2 v2.2.2 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/crypto v0.34.0 // indirect
-	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/text v0.22.0 // indirect
-	gorm.io/driver/postgres v1.5.11
-	gorm.io/gorm v1.25.12
+	github.com/golang/snappy v0.0.3 // indirect
+	github.com/hailocab/go-hostpool v0.0.0-20160125115350-e80d13ce29ed // indirect
+	github.com/kr/pretty v0.3.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 )
+
+require golang.org/x/text v0.22.0 // indirect
